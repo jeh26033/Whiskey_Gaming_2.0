1e049d1b3c5f8515850253d8e50cae3f34116562
joseph <jeh26033@email.vccs.edu>
2018-01-16 17:36:22 -0500

    take2

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 config.serve_static_assets = true
+config.load_defaults 5.1
 module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
